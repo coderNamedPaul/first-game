5f37de36167c9dd419d88eeba482bf90570183f1
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2021-03-22 22:20:46 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from graph import *
+import tkinter as tk
+
+root = mainWindow()
+root.title('test')
+windowSize(600, 600)
+canvasSize(1920, 1080)
+
+text('test', 290, 300)
+button('test button', 280, 200,)
+
+run()
